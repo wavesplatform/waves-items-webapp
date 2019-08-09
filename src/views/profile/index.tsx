b47b94a7f338c05f64237058c3a85da29594f645
Vecheslav Druzhbin <vecheslavdruzhbin@gmail.com>
2019-08-09 17:12:41 +0300

    inventory bug fix

--- src/views/profile/index.tsx
+++ src/views/profile/index.tsx
@@ -1,12 +1,17 @@
 import React, { Component, ReactNode } from 'react'
-import { Container, Section, ViewWrapper } from '../../components/layout'
-import { H1, H2 } from '../../components/globals'
+import { Container, ViewWrapper } from '../../components/layout'
+import { H1 } from '../../components/globals'
 import Inventory from '../inventory'
 import withCurrentUser, { WithCurrentUserProps } from '../../components/withCurrentUser'
+import { Redirect } from 'react-router'
 
 class Profile extends Component<WithCurrentUserProps> {
   render(): ReactNode {
     const { me } = this.props
+
+    if (!me) {
+      return <Redirect to={'/signin'}/>
+    }
 
     return (
       <ViewWrapper>
