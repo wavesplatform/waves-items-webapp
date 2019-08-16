6f470703aa554d1c6675548d07bc83a5f4a9c07f
Vecheslav Druzhbin <vecheslavdruzhbin@gmail.com>
2019-08-16 20:05:08 +0300

    assign filter params to game nav

--- src/views/gameNav/components/gameList.tsx
+++ src/views/gameNav/components/gameList.tsx
@@ -1,22 +1,26 @@
 import { IGame } from '../../../types'
-import { Link } from 'react-router-dom'
+import { Link, RouteComponentProps, withRouter } from 'react-router-dom'
 import React from 'react'
 import { ListContainer } from '../../../components/listItems/style'
 import { GameListItem } from '../../../components/listItems'
 import { UserHeading } from '../../../components/user/userHeading'
 import withCurrentUser, { WithCurrentUserProps } from '../../../components/withCurrentUser'
 import { UserRole } from '../../../__generated__/globalTypes'
+import { compose } from 'react-apollo'
 
 type TProps = {
   games: IGame[]
 }
 
-const GameList = (props: WithCurrentUserProps<TProps>) => {
-  const { me } = props
+const GameList = (props: WithCurrentUserProps<TProps & RouteComponentProps>) => {
+  const { me, location } = props
 
   const games = props.games.map(game => (
     <GameListItem as={Link}
-                  to={`/items/${game.address}`}
+                  to={{
+                    pathname: `/items/${game.address}`,
+                    search: location.search,
+                  }}
                   key={game.id}>
       <UserHeading user={game} size={'base'}/>
     </GameListItem>
@@ -24,10 +28,16 @@ const GameList = (props: WithCurrentUserProps<TProps>) => {
 
   return (
     <ListContainer>
-      <GameListItem as={Link} to={'/items'}>
+      <GameListItem as={Link} to={{
+        pathname: '/items',
+        search: location.search,
+      }}>
         All Games
       </GameListItem>
-      {me && me.role === UserRole.TEST && <GameListItem as={Link} to={`/items/${me.address}`}>
+      {me && me.role === UserRole.TEST && <GameListItem as={Link} to={{
+        pathname: `/items/${me.address}`,
+        search: location.search,
+      }}>
         <UserHeading user={me} size={'base'}/>
       </GameListItem>}
       {games}
@@ -35,4 +45,4 @@ const GameList = (props: WithCurrentUserProps<TProps>) => {
   )
 }
 
-export default withCurrentUser(GameList)
+export default compose(withCurrentUser, withRouter)(GameList)
